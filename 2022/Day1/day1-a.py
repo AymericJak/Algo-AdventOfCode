6e6a93e44874c3d46ca6f9a1c62ed728a8e30787
AymericJak <aymerictravail129@gmail.com>
2022-12-04 18:10:07 +0100

    2022-Day1-B

--- 2022/Day1/day1-a.py
+++ 2022/Day1/day1-a.py
@@ -1,11 +1,14 @@
+from collections import Counter
+
+# Part A
 with open("day1.txt", 'r') as file:
     data = file.read()
 
-otherData = data.split('\n')
+listData = data.split('\n')
 elfes = {}
 index = 1
 
-for elem in otherData:
+for elem in listData:
     if elem == '':
         index += 1
     else:
@@ -14,6 +17,14 @@ for elem in otherData:
         else:
             elfes[index] = int(elem)
 
-
 print(max(elfes.values()))
-#print(max(elfes, key=elfes.get))
+
+# Part B
+
+max_values = Counter(elfes).most_common(3)
+
+sum_max_values = 0
+for i in max_values:
+    sum_max_values += i[1]
+
+print(sum_max_values)
