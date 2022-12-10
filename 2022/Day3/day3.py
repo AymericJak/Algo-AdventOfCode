152596e9f33932a48be907b7060d22240853515f
AymericJak <aymerictravail129@gmail.com>
2022-12-10 18:20:50 +0100

    2022-Day3-B

--- 2022/Day3/day3.py
+++ 2022/Day3/day3.py
@@ -5,6 +5,7 @@ with open("day3-a.txt", 'r') as file:
 
 # PART A
 
+lettersValue = {letter:i+1 for i,letter in enumerate(string.ascii_letters)}
 commonLetters = []
 
 for item in data:
@@ -13,10 +14,33 @@ for item in data:
     part2 = set(item[itemLength:])
     commonLetters.append(part1.intersection(part2).pop())
 
-lettersValue = {letter:i+1 for i,letter in enumerate(string.ascii_letters)}
 
 somme = 0
 for letter in commonLetters:
     somme += lettersValue.get(letter)
 
 print(somme)
+
+# PART B
+
+commonLetters2 = []
+
+
+for index, item in enumerate(data):
+    index += 1
+    item = set(item)
+
+    if index % 3 == 1:
+        liste = [item]
+    elif index % 3 == 2:
+        liste.append(item)
+    elif index % 3 == 0:
+        liste.append(item)
+        commonLetters2.append((liste[0] & liste[1] & liste[2]).pop())
+
+
+sommeB = 0
+for letter in commonLetters2:
+    sommeB += lettersValue.get(letter)
+
+print(sommeB)
