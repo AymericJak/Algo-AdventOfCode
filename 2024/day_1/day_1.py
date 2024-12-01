683384685ac7f7537e97033d609c11f25eb9748c
AymericJak <aymericjakpro@gmail.com>
2024-12-01 13:20:04 +0100

    2024 - 1st december - problem B

--- 2024/day_1/day_1.py
+++ 2024/day_1/day_1.py
@@ -1,3 +1,5 @@
+import collections
+
 example = """3   4
 4   3
 2   5
@@ -6,6 +8,7 @@ example = """3   4
 3   3"""
 
 with open("input_day_1.txt", 'r') as file:
+    # data = [line.split() for line in example.strip().split("\n")]
     data = [line.split() for line in file.read().strip().split("\n")]
 
 # PART A
@@ -14,7 +17,17 @@ left_numbers = sorted([int(line[0]) for line in data])
 right_numbers = sorted([int(line[1]) for line in data])
 distances = 0
 
-for index in range(len(right_numbers)):
+for index in range(len(left_numbers)):
     distances += abs(left_numbers[index] - right_numbers[index])
 
-print(distances)
+print("Part A answer :", distances)
+
+# PART B
+
+similarity_score = 0
+
+for index in range(len(left_numbers)):
+    counter = collections.Counter(right_numbers)
+    similarity_score += left_numbers[index] * counter[left_numbers[index]]
+
+print("Part B answer :", similarity_score)
