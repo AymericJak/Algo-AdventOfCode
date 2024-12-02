32a4747f71921f7194667f970cd1e2e14bf0b16e
AymericJak <aymericjakpro@gmail.com>
2024-12-02 09:18:38 +0100

    2024 - 2nd december - problem B

--- 2024/day_2/day_2.py
+++ 2024/day_2/day_2.py
@@ -12,6 +12,13 @@ with open("input_day_2.txt", 'r') as file:
     data = [list(map(int, line.split())) for line in file.read().strip().split("\n")]
 
 
+def is_valid_report(report):
+    gaps = [a - b for a, b in itertools.pairwise(report)]
+
+    if (all(gap > 0 for gap in gaps) or all(gap < 0 for gap in gaps)) and all(abs(gap) <= 3 and gap != 0 for gap in gaps):
+        return True
+    return False
+
 
 # PART A
 
@@ -19,10 +26,25 @@ with open("input_day_2.txt", 'r') as file:
 nb_safe_reports = 0
 
 for report in data:
-    gaps = [a - b for a, b in itertools.pairwise(report)]
-
-    if all(gap > 0 for gap in gaps) or all(gap < 0 for gap in gaps):
-        if all([i in {nb for nb in range(-3, 4)} for i in gaps]):
-            nb_safe_reports += 1
+    if is_valid_report(report):
+        nb_safe_reports += 1
 
 print("Part A answer :", nb_safe_reports)
+
+
+# PART B
+
+
+nb_safe_reports = 0
+
+for report in data:
+    if is_valid_report(report):
+        nb_safe_reports += 1
+    else:
+        for i in range(len(report)):
+            modified_report = report[:i] + report[i+1:]
+            if is_valid_report(modified_report):
+                nb_safe_reports += 1
+                break
+
+print("Part B answer :", nb_safe_reports)
