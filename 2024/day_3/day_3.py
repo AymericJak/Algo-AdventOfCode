f2ff48ad41b93ea97299503f0e61173207e3dbca
AymericJak <aymericjakpro@gmail.com>
2024-12-05 07:14:39 +0100

    2024 - 3rd december - problem B

--- 2024/day_3/day_3.py
+++ 2024/day_3/day_3.py
@@ -15,3 +15,22 @@ for res in search_result:
     sum_multiplies += first_nb * second_nb
 
 print("Part A answer :", sum_multiplies)
+
+# PART B
+
+example_2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
+search_result = re.findall("mul\(\d+,\d+\)|do\(\)|don\'t\(\)", data)
+sum_multiplies = 0
+is_doing = True
+
+for res in search_result:
+    if res == "don't()":
+        is_doing = False
+    elif res == "do()":
+        is_doing = True
+    elif is_doing:
+        first_nb, second_nb = map(int, res[4:-1].split(","))
+        sum_multiplies += first_nb * second_nb
+
+print("Part B answer :", sum_multiplies)
