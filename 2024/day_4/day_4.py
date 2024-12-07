9b3b496763337be52d727bb47fcdad0d042a09f4
AymericJak <aymericjakpro@gmail.com>
2024-12-07 21:40:13 +0100

    2024 - 4th december - problem B

--- 2024/day_4/day_4.py
+++ 2024/day_4/day_4.py
@@ -1,23 +1,6 @@
 import re
 import numpy as np
 
-should_4 = """..X...
-.SAMX.
-.A..A.
-XMAS.S
-.X...."""
-
-example = """MMMSXXMASM
-MSAMXMSMSA
-AMXSXMAAMM
-MSAMASMSMX
-XMASAMXAMM
-XXAMMXXAMA
-SMSMSASXSS
-SAXAMASAAA
-MAMMMXMMMM
-MXMXAXMASX"""
-
 with open("input_day_4.txt", 'r') as file:
     data = file.read()
 
@@ -28,20 +11,20 @@ def count_word(text: str, word: str) -> int:
     return len(re.findall(word, text)) + len(re.findall(word[::-1], text))
 
 
-def search_horizontaly(text: str) -> int:
-    return count_word(text, "XMAS")
+def search_horizontaly(text: str, word: str) -> int:
+    return count_word(text, word)
 
 
-def search_verticaly(text: str) -> int:
+def search_verticaly(text: str, word: str) -> int:
     rows = text.split("\n")
     numpy_matrix = np.array([list(row) for row in rows])
     transposed_matrix = numpy_matrix.T
 
     transposed_text = "\n".join("".join(row) for row in transposed_matrix)
-    return count_word(transposed_text, "XMAS")
+    return count_word(transposed_text, word)
 
 
-def search_diagonaly(text: str) -> int:
+def search_diagonaly(text: str, word: str) -> int:
     rows = text.split("\n")
     numpy_matrix = np.array([list(row) for row in rows])
 
@@ -57,17 +40,44 @@ def search_diagonaly(text: str) -> int:
     ]
 
     diagonal_text = "\n".join(diagonals)
-    return count_word(diagonal_text, "XMAS")
+    return count_word(diagonal_text, word)
 
 
-def calculate_xmas_occurs(text: str) -> int:
+def calculate_word_occurs(text: str, word: str) -> int:
     return (
-            search_horizontaly(text)
-            + search_verticaly(text)
-            + search_diagonaly(text)
+            search_horizontaly(text, word)
+            + search_verticaly(text, word)
+            + search_diagonaly(text, word)
     )
 
 
-print("should_4 :", calculate_xmas_occurs(should_4))
-print("example_shoul_18 :", calculate_xmas_occurs(example))
-print("Part A answer :", calculate_xmas_occurs(data))
+print("Part A answer :", calculate_word_occurs(data, "XMAS"))
+
+
+# PART B
+
+def calculte_cross_mas(text: str) -> int:
+    count = 0
+    text_array = text.split("\n")
+    patterns = {
+        ("M", "M", "S", "S"),
+        ("S", "S", "M", "M"),
+        ("M", "S", "M", "S"),
+        ("S", "M", "S", "M"),
+    }
+
+    for y in range(1, len(text_array) - 1):
+        for x in range(1, len(text_array[y]) - 1):
+            if text_array[x][y] == "A":
+                top_left = text_array[x - 1][y - 1]
+                bottom_left = text_array[x + 1][y - 1]
+                top_right = text_array[x - 1][y + 1]
+                bottom_right = text_array[x + 1][y + 1]
+
+                if (top_left, top_right, bottom_left, bottom_right) in patterns:
+                    count += 1
+
+    return count
+
+
+print("Part B answer :", calculte_cross_mas(data))
