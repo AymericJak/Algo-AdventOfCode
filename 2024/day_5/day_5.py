e5e4d4b96317ab10e7425409346c134a6c4260a2
AymericJak <aymericjakpro@gmail.com>
2024-12-08 11:56:34 +0100

    2024 - 5th december - problem B

--- 2024/day_5/day_5.py
+++ 2024/day_5/day_5.py
@@ -1,38 +1,8 @@
-example = """47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
-
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47"""
-
 with open("input_day_5.txt", 'r') as file:
     data = file.read()
 
 # PART A
 
-# data_first_section, data_second_section = example.split("\n\n")
 data_first_section, data_second_section = data.split("\n\n")
 data_first_section = data_first_section.split("\n")
 data_second_section = data_second_section.split("\n")
@@ -59,21 +29,46 @@ def check_update_order(rules: dict, updates: list) -> bool:
     return True
 
 
-def filter_all_updates_order(rules: dict, data_second_section: list) -> list:
-    """Filter valid updates validating rules."""
-    correct_updates = []
+def filter_updates_order(rules: dict, data_second_section: list, is_correct: bool = True) -> list:
+    """Filter updates validating rules corresponding to is_correct."""
+    updates = []
     for update in data_second_section:
         update_row = list(map(int, update.split(",")))
-        if check_update_order(rules, update_row):
-            correct_updates.append(update_row)
-    return correct_updates
+        if check_update_order(rules, update_row) == is_correct:
+            updates.append(update_row)
+    return updates
 
 
-def calculate_middle_pages_sum(correct_updates: list) -> int:
+def calculate_middle_pages_sum(updates: list) -> int:
     """Calculate sum of middle pages of valid updates."""
-    return sum([update[len(update) // 2] for update in correct_updates])
+    return sum([update[len(update) // 2] for update in updates])
 
 
 rules = generate_rules(data_first_section)
-correct_updates = filter_all_updates_order(rules, data_second_section)
+correct_updates = filter_updates_order(rules, data_second_section)
 print("Part A answer :", calculate_middle_pages_sum(correct_updates))  # 4135
+
+
+# PART B
+
+
+def fix_update(rules: dict, incorrect_updates: list) -> list:
+    """Fix incorrect updates."""
+    fixed_updates = []
+
+    for update in incorrect_updates:
+        fixed_update = []
+        for page in update:
+            for i in range(len(fixed_update) + 1):
+                # Check if update is still valid after insertion
+                temp_update = fixed_update[:i] + [page] + fixed_update[i:]
+                if check_update_order(rules, temp_update):
+                    fixed_update = temp_update
+                    break
+        fixed_updates.append(fixed_update)
+    return fixed_updates
+
+
+incorrect_updates = filter_updates_order(rules, data_second_section, False)
+fixed_updates = fix_update(rules, incorrect_updates)
+print("Part B answer :", calculate_middle_pages_sum(fixed_updates))  # 5285
